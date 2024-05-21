92edaf264a2d8b34a5087005b78c9566b17fd804
Bruno <bsaragao1990@gmail.com>
2024-05-20 23:44:39 -0300

    Update conexao.php

--- atividade6/conexao.php
+++ atividade6/conexao.php
@@ -1,12 +1,12 @@
 <?php
-    $local = "localhost";
-    $user "root";
-    $pass = "";
-    $bd = "crud";
+    $local='localhost';
+    $user='root';
+    $pass='';
+    $bd='cnpj';
 
-    $mysqli = mysqli_connect($local,$user,$pass,$bd); 
-    
+    $mysqli = mysqli_connect($local,$user,$pass,$bd);
+
     if ($mysqli->connect_error) {
-        die("Erro na conexão com o banco de dados: " . $mysqli->connect_error);
-    }
+            die("Erro na conexão com o banco de dados: " . $mysqli->connect_error);
+        }
 ?>
